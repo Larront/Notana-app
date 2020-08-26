2a3280ef64159a121dbb2f61367c6915c15e1d75
Aaron Lamont <aaronlamont27@gmail.com>
2020-08-26 21:41:02 +1200

    Re-implement basic PDF Viewer. Re-Implement Basic InitTracker

---
+++ src/components/PdfViewer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <pdf
+      ref="pdf"
+      :src="src"
+      :page="page"
+      @num-pages="numPages = $event"
+      @link-clicked="page = $event"
+    ></pdf>
+    <input v-model.number="page" type="number" style="width: 5em" />
+    /{{numPages}}
+    <div style="width: 50%">
+      <div
+        v-if="loadedRatio > 0 && loadedRatio < 1"
+        style="background-color: green; color: white; text-align: center"
+        :style="{ width: loadedRatio * 100 + '%' }"
+      >{{ Math.floor(loadedRatio * 100) }}%</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import pdf from "vue-pdf";
+
+export default {
+  components: {
+    pdf,
+  },
+  data() {
+    return {
+      src: "./docs/5E_Wolves_Of_Welton.pdf",
+      loadedRatio: 0,
+      page: 1,
+      numPages: 0,
+      rotate: 0,
+    };
+  },
+};
+</script>
